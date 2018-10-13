9d7f1865bf4e93aa1b000a385bbc00c41bc7b7c2
Alex White <alex.white@daugherty.com>
2018-10-13 15:12:42 -0500

    Get full auth working end-to-end with auth0

---
+++ src/main/java/com/gh7/api/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.gh7.api.config;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private String apiAudience;
+  private String issuer;
+
+  @Autowired
+  public SecurityConfig(@Value(value = "${auth0.apiAudience}") String apiAudience, @Value(value = "${auth0.issuer}") String issuer) {
+    this.apiAudience = apiAudience;
+    this.issuer = issuer;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors();
+    JwtWebSecurityConfigurer
+        .forRS256(apiAudience, issuer)
+        .configure(http)
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/users*").authenticated()
+        .antMatchers(HttpMethod.GET, "/users/*").authenticated()
+        .antMatchers(HttpMethod.POST, "/users*").permitAll()
+        .antMatchers(HttpMethod.PUT, "/users**").authenticated()
+
+        .antMatchers(HttpMethod.POST, "/assistance**").authenticated()
+
+        .antMatchers(HttpMethod.POST, "/twilio**").permitAll();
+  }
+
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//    configuration.setAllowCredentials(true);
+//    configuration.addAllowedHeader("Authorization");
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+}
