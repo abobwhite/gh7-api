9b7b565840dbb174aecb706f7ce8e1183e9a563f
Alex White <alex.white@daugherty.com>
2018-10-13 09:15:16 -0500

    Bye Bye Groovy :(

---
+++ src/main/groovy/com/gh7/api/Application.java
@@ -0,0 +1,11 @@
+package com.gh7.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
